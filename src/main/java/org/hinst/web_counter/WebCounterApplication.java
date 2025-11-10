85f83c57344851f4a9435a22956eba3f1e306759
hinst <hinstance@yandex.ru>
2025-11-10 23:19:17 +0100

    Adding initial project files from Spring Initializer

---
+++ src/main/java/org/hinst/web_counter/WebCounterApplication.java
@@ -0,0 +1,13 @@
+package org.hinst.web_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hinst/web_counter/WebCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hinst.web_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
