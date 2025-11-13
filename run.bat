84ddab2c1d17236dca7a9255d616d2583ee1c6cf
hinst <hinstance@yandex.ru>
2025-11-13 20:48:06 +0100

    run CALL mvn, because otherwise batch file will exit and no longer run commands coming after mvn

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 set JAVA_HOME=C:\Users\hinst\.jdks\temurin-21.0.5
-mvn clean package -DskipTests
+CALL mvn clean package -DskipTests
 C:\Users\hinst\.jdks\temurin-21.0.5\bin\java -jar target\web-counter-0.0.1-SNAPSHOT.jar
