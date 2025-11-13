06e92d099057160405e21a404367aefc59450ed2
hinst <hinstance@yandex.ru>
2025-11-13 20:57:53 +0100

    Adding configuration parameter: applicationName

--- src/main/java/org/hinst/web_counter/AboutController.java
+++ src/main/java/org/hinst/web_counter/AboutController.java
@@ -1,10 +1,14 @@
 package org.hinst.web_counter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AboutController {
+	@Value("${spring.application.name}")
+	private String applicationName;
+
 	@RequestMapping("/about")
 	public String index() {
 		return "about.html";
