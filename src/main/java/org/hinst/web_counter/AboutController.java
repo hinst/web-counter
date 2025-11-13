e8c1cb330d13cd8130bbee2d12d09aaccaa5d1a0
hinst <hinstance@yandex.ru>
2025-11-13 20:48:21 +0100

    Adding about.html

---
+++ src/main/java/org/hinst/web_counter/AboutController.java
@@ -0,0 +1,12 @@
+package org.hinst.web_counter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AboutController {
+	@RequestMapping("/about")
+	public String index() {
+		return "about.html";
+	}
+}
--- src/main/java/org/hinst/web_counter/WebCounterApplication.java
+++ src/main/java/org/hinst/web_counter/WebCounterApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebCounterApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebCounterApplication.class, args);
 	}
-
 }
