dd61e14527e117db462aa07852c1f94fe5e847c6
hinst <hinstance@yandex.ru>
2025-11-15 21:51:34 +0100

    Use specific HTTP method mapping for /about

--- src/main/java/org/hinst/web/counter/AboutController.java
+++ src/main/java/org/hinst/web/counter/AboutController.java
@@ -2,14 +2,14 @@ package org.hinst.web.counter;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AboutController {
 	@Value("${spring.application.name}")
 	private String applicationName;
 
-	@RequestMapping("/about")
+	@GetMapping("/about")
 	public String index() {
 		return "about.html";
 	}
