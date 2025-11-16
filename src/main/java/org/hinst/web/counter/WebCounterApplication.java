802d9b959f937a4c9bca8affc81b79f06bd49178
hinst <hinstance@yandex.ru>
2025-11-16 18:33:16 +0100

    Highlight log messages coming from my package. Print timezone information after startup

--- src/main/java/org/hinst/web/counter/WebCounterApplication.java
+++ src/main/java/org/hinst/web/counter/WebCounterApplication.java
@@ -1,11 +1,24 @@
 package org.hinst.web.counter;
 
+import java.time.ZonedDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class WebCounterApplication {
+	private Logger logger = LoggerFactory.getLogger(WebCounterApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebCounterApplication.class, args);
+	}
+
+	@PostConstruct
+	public void runAfterStartup() {
+		logger.info("Started in timezone: {} {}", ZonedDateTime.now().getZone(), ZonedDateTime.now().getOffset());
 	}
 }
