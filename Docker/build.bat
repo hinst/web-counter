070bc42a092feb16809ae7c644215f5cb5c6887f
hinst <hinstance@yandex.ru>
2025-11-30 13:18:04 +0100

    Move Dockerfile into folder Docker

--- Docker/build.bat
+++ Docker/build.bat
@@ -1,1 +1,1 @@
-docker build -t hinst/web-counter --progress=plain --platform=linux/arm64 .  || exit /b
+docker build -t hinst/web-counter --progress=plain --platform=linux/arm64 -f Docker/Dockerfile .  || exit /b
