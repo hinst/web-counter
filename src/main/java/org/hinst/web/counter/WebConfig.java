02fad5900a628f264bcece05cbeeb43ab14520f7
hinst <hinstance@yandex.ru>
2025-11-30 18:54:08 +0100

    Add NonNull to avoid compiler warning

--- src/main/java/org/hinst/web/counter/WebConfig.java
+++ src/main/java/org/hinst/web/counter/WebConfig.java
@@ -1,12 +1,13 @@
 package org.hinst.web.counter;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**");
     }
 }
